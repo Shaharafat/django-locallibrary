396a20055cb0ee8888ab6e71ebc579735205a063
shah_arafat <shah@co.design>
2022-04-14 22:26:21 +0600

    adding forms

--- catalog/urls.py
+++ catalog/urls.py
@@ -13,3 +13,9 @@ urlpatterns = [
 urlpatterns += [
     path("mybooks/", views.LoanedBooksByUserListView.as_view(), name="my-borrowed")
 ]
+
+urlpatterns += [
+    path(
+        "book/<uuid:pk>/renew/", views.renew_book_librarian, name="renew-book-librarian"
+    ),
+]
